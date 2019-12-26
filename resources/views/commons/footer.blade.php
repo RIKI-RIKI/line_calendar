4a36165eb7153a693fa84c5e9390e1734d10c280
EC2 Default User <ec2-user@ip-172-31-23-170.us-east-2.compute.internal>
2019-12-26 09:17:50 +0000

    検索機能

--- resources/views/commons/footer.blade.php
+++ resources/views/commons/footer.blade.php
@@ -1,8 +1,6 @@
-<footer class="page-footer font-small special-color-dark pt-4"　style="background-color: aquamarine;">
-
-            <!-- Footer Elements -->
+<footer class="page-footer font-small special-color-dark pt-4" style="background-color: aquamarine;">
         <div class="container">
-            <!-- Social buttons -->
+            
             <ul class="list-unstyled list-inline text-center" style = "display: flex;">
                 <div class = twitters style="margin: 0 auto; display: flex;">
                     <div class = "p-2">
@@ -22,25 +20,8 @@
                         </li>
                     </div>
                 </div>
-            <!-- <li class="list-inline-item">
-                <a class="btn-floating btn-gplus mx-1">
-                <i class="fab fa-google-plus-g"> </i>
-                </a>
-            </li>
-            <li class="list-inline-item">
-                <a class="btn-floating btn-li mx-1">
-                <i class="fab fa-linkedin-in"> </i>
-                </a>
-            </li>
-            <li class="list-inline-item">
-                <a class="btn-floating btn-dribbble mx-1">
-                <i class="fab fa-dribbble"> </i>
-                </a> 
-            </li>-->
             </ul>
-            <!-- Social buttons -->        
+            
         </div>
-        <!-- Footer Elements -->
-        <!-- Copyright -->
-        <div class="footer-copyright text-center py-3">© 2020 Nanaka&Riki All Rights Reserved.</div>
-    <!-- Copyright -->    
+        <div class="footer-copyright text-center py-3" >© 2020 Nanaka&Riki All Rights Reserved.</div>
+</footer>
